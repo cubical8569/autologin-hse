f7cd632236be35ca6086e9c0b9e2887598a9f933
CookiesDeathCookies <azotx12345@gmail.com>
2019-09-11 17:58:19 +0300

    Add HSE network

---
+++ 02-autologin-hse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$1 $2" = 'wlp2s0 up' ]
+then
+    essid=$(iwconfig wlp2s0 | grep -o 'ESSID:".*$' | sed 's/^ESSID:"\(.*\)".*$/\1/')
+    if [ "$essid" = 'HSE' ]
+    then
+        POST 192.0.2.1/login.html <<< 'username=hseguest&password=hsepassword'
+    fi
+fi
---
+++ setup.sh
@@ -0,0 +1,2 @@
+chmod +x 02-autologin-hse.sh
+cp 02-autologin-hse.sh /etc/NetworkManager/dispatcher.d  
