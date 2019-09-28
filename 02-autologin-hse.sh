f24b19ca438b8d336be3231c981ce2015fd3ebcb
CookiesDeathCookies <azotx12345@gmail.com>
2019-09-28 11:41:06 +0300

    Fix typo

--- 02-autologin-hse.sh
+++ 02-autologin-hse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1 $2" = 'wlp2s0 up' ] || [ "$2" == "connectivity-change" ]
+if [ "$1 $2" = 'wlp2s0 up' ] || [ "$2" = "connectivity-change" ]
 then
     essid=$(iwconfig wlp2s0 | grep -o 'ESSID:".*$' | sed 's/^ESSID:"\(.*\)".*$/\1/')
     if [ "$essid" = 'HSE' ]
